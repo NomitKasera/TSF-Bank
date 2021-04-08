944638f826dbaac4666b71b052c5406049ff1fdd
Nomit <53518528+NomitKasera@users.noreply.github.com>
2021-04-08 19:39:39 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,30 @@
+<!-- navbar --> 
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"> <img src="img/logo_small.png" width="35" height="auto"> TSF Bank </img></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><i class="fa fa-home"></i> Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><i class="fa fa-user-plus"></i> Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><i class="fa fa-credit-card" aria-hidden="true"></i> Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><i class="fa fa-history" aria-hidden="true"></i> Transaction History</a>
+              </li>
+          </div>
+       </nav>
+       </body>
+</html>
